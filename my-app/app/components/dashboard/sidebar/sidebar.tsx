fd21ee20b518d050a59b25d6da3412fc061f6295
William Giammona <wgiammona@gmail.com>
2024-02-05 16:16:16 +0200

    added styling

--- my-app/app/components/dashboard/sidebar/sidebar.tsx
+++ my-app/app/components/dashboard/sidebar/sidebar.tsx
@@ -1,5 +1,7 @@
+import styles from "./sidebar.module.css";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return <div className={styles.container}>Sidebar</div>;
 };
 
 export default Sidebar;
