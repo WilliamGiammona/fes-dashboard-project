1a65d8665914a554474895cf26abd928c1cf3756
William Giammona <wgiammona@gmail.com>
2024-02-06 14:31:30 +0200

    finished layout

--- my-app/app/components/dashboard/card/card.tsx
+++ my-app/app/components/dashboard/card/card.tsx
@@ -5,6 +5,14 @@ const Card = () => {
   return (
     <div className={styles.container}>
       <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>Total Users</span>
+        <span className={styles.number}>10.283</span>
+        <span className={styles.detail}>
+          <span className={styles.positive}>11%</span>
+          more than previous week
+        </span>
+      </div>
     </div>
   );
 };
