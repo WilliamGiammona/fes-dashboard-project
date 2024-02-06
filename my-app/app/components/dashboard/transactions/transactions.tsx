67ebde0a1e03d96697dfbe24ff770b29212ea5ae
William Giammona <wgiammona@gmail.com>
2024-02-06 14:39:11 +0200

    creating layout and table

--- my-app/app/components/dashboard/transactions/transactions.tsx
+++ my-app/app/components/dashboard/transactions/transactions.tsx
@@ -1,7 +1,15 @@
 import styles from "./transactions.module.css";
 
 const Transactions = () => {
-  return <div>Transactions</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Latest Transactions</h2>
+      <table className={styles.table}>
+        <thead></thead>
+        <tbody></tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Transactions;
