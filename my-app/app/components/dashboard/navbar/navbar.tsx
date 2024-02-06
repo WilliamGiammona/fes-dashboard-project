7fbd58f133aa00c2e11bacbc70b137ac23290181
William Giammona <wgiammona@gmail.com>
2024-02-06 13:38:06 +0200

    imported react-icons

--- my-app/app/components/dashboard/navbar/navbar.tsx
+++ my-app/app/components/dashboard/navbar/navbar.tsx
@@ -2,6 +2,12 @@
 
 import styles from "./navbar.module.css";
 import { usePathname } from "next/navigation";
+import {
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+  MdSearch,
+} from "react-icons/md";
 
 const Navbar = () => {
   const pathname = usePathname();
