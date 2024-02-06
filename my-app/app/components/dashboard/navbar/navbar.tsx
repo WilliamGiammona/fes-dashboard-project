c574657a4f55a774c858e2399f591eb47ad629db
William Giammona <wgiammona@gmail.com>
2024-02-06 13:36:56 +0200

    imported usePathname hook

--- my-app/app/components/dashboard/navbar/navbar.tsx
+++ my-app/app/components/dashboard/navbar/navbar.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import styles from "./navbar.module.css";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
   return <div className={styles.container}>Navbar</div>;
 };
 
