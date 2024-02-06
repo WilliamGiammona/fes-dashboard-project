fe1084ae79df44e602dfb677dff9a350d290f041
William Giammona <wgiammona@gmail.com>
2024-02-06 14:28:39 +0200

    adding icon

--- my-app/app/components/dashboard/card/card.tsx
+++ my-app/app/components/dashboard/card/card.tsx
@@ -2,7 +2,11 @@ import styles from "./card.module.css";
 import { MdSupervisedUserCircle } from "react-icons/md";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={24} />
+    </div>
+  );
 };
 
 export default Card;
